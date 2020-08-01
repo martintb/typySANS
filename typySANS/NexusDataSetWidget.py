25819c754e5f09074f95cb8e7161804dbee9b00a
Tyler Martin <tyler.martin@nist.gov>
2020-08-01 10:17:20 -0400

    Plot prev/next buttons working

--- typySANS/NexusDataSetWidget.py
+++ typySANS/NexusDataSetWidget.py
@@ -20,23 +20,41 @@ class NexusDataSetWidget:
         self.selected_row = None
     
     def load_files(self,*args):
-        data_path = self.data_view.data_path.value
-        filedata = self.data_model.get_filedata(data_path)
+        load_path = self.data_view.load_path.value
+        filedata = self.data_model.get_filedata(load_path)
         self.data_view.update_grid(filedata)
         
+    def plot_increment(self,button):
+        grid_data_out = self.data_view.grid.grid_data_out
+        try:
+            selected_rows =  grid_data_out['rows'].iloc[0]
+        except KeyError:
+            selected_rows =  grid_data_out['grid'].iloc[0]
+        
+        if button.description == 'PLOT NEXT':
+            index = selected_rows.name[0]+1
+        elif button.description == 'PLOT PREV':
+            index = selected_rows.name[0]-1
+        if index>grid_data_out['grid'].index.max()[0]:
+            index = grid_data_out['grid'].index.min()[0]
+        elif index<grid_data_out['grid'].index.min()[0]:
+            index = grid_data_out['grid'].index.max()[0]
+        
+        grid_data_out['rows'] = grid_data_out['grid'].iloc[[index]]
+        self.plot_data()
+            
     def plot_data(self,*args):
         try:
-            selected_rows =  self.data_view.grid.grid_data_out['rows']
+            selected_rows =  self.data_view.grid.grid_data_out['rows'].iloc[0]
         except KeyError:
-            #no rows selected
             return
         
-        filename = selected_rows.iloc[0]['filename']#.squeeze()
+        filename = selected_rows['filename']#.squeeze()
         self.selected_row = self.data_view.grid.grid_data_out['rows'].iloc[0].name[0]
             
             
-        data_path = pathlib.Path(self.data_view.data_path.value)
-        filepath = data_path/filename
+        load_path = pathlib.Path(self.data_view.load_path.value)
+        filepath = load_path/filename
         with h5py.File(filepath,'r') as h5:
             self.selected_img = h5['entry/data/y'][()].T
         self.data_view.image.update_widget_image(self.selected_img)
@@ -53,9 +71,9 @@ class NexusDataSetWidget:
             warnings.warn('Skipping move as destination is a file!')
         move_path.mkdir(exist_ok=True)
         
-        data_path = pathlib.Path(self.data_view.data_path.value)
+        load_path = pathlib.Path(self.data_view.load_path.value)
         for fname in selected_rows['filename']:
-            old_path = data_path/fname
+            old_path = load_path/fname
             new_path = move_path/fname
             
             old_path.replace(new_path)#actually does the move
@@ -68,6 +86,8 @@ class NexusDataSetWidget:
         self.data_view.load_button.on_click(self.load_files)
         self.data_view.move_button.on_click(self.move)
         self.data_view.plot_button.on_click(self.plot_data)
+        self.data_view.plot_next_button.on_click(self.plot_increment)
+        self.data_view.plot_prev_button.on_click(self.plot_increment)
         return widget
         
                 
@@ -79,10 +99,10 @@ class NexusDataSetWidget_DataModel:
         self.path = pathlib.Path(path)
         
         filedata = []
-        for fname in self.path.glob('*nxs*'):
-            with h5py.File(fname,'r') as h5:
+        for file in sorted(self.path.glob('*nxs*')):
+            with h5py.File(file,'r') as h5:
                 filedata.append({
-                    'filename':str(fname.parts[-1]),
+                    'filename':str(file.parts[-1]),
                     'label':h5['entry/sample/description'][()][0].decode('utf8'),
                     'countTime':'{:.2f}'.format(float(h5['entry/collection_time'][()][0])),
                     'detectorDistance':'{:5.2f}'.format(h5['entry/DAS_logs/detectorPosition/softPosition'][()][0]),
@@ -118,10 +138,11 @@ class NexusDataSetWidget_DataView:
             index=True,
             quick_filter=True, 
             export_mode='auto',
+            show_toggle_edit=True,
             theme='ag-theme-balham', 
         )
         self.load_button = ipywidgets.Button(description='LOAD FILES')
-        self.data_path     = ipywidgets.Text(value='./')
+        self.load_path     = ipywidgets.Text(value='./')
         self.move_button   = ipywidgets.Button(description='MOVE SELECTED')
         self.move_path     = ipywidgets.Text(value='./junk/')
         
@@ -131,7 +152,7 @@ class NexusDataSetWidget_DataView:
         
         self.image = ImageWidget(2.0*np.ones((128,128)))
         
-        down_hbox = ipywidgets.HBox([self.load_button,self.data_path])
+        down_hbox = ipywidgets.HBox([self.load_button,self.load_path])
         move_hbox = ipywidgets.HBox([self.move_button,self.move_path])
         plot_hbox = ipywidgets.HBox([self.plot_button,self.plot_prev_button,self.plot_next_button])
         vbox = ipywidgets.VBox([
@@ -146,5 +167,3 @@ class NexusDataSetWidget_DataView:
         
     
     
-        
-        
