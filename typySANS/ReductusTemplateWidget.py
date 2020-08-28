dad4b1609be4cd601705955aac758b00f24ee19e
Tyler Martin <tyler.martin@nist.gov>
2020-08-28 11:30:54 -0400

    ReductusTemplateWidget Cleanup
    - DataView methods refactored
    - visualization cleaned up and modified
    - added ipyaggrid for input file visualization

--- typySANS/ReductusTemplateWidget.py
+++ typySANS/ReductusTemplateWidget.py
@@ -9,20 +9,13 @@ import json
 
 import plotly.subplots
 import plotly.graph_objs as go
+import ipyaggrid
 
 class ReductusTemplateWidget:
     def __init__(self,template_path):
         self.selected_node = 0
         self.data_model = ReductusTemplateWidget_DataModel(template_path)
         self.data_view = ReductusTemplateWidget_DataView()
-        self.data_view.init_graph(
-            self.data_model.nodes_x,
-            self.data_model.nodes_y,
-            self.data_model.edges_x,
-            self.data_model.edges_y,
-            self.data_model.labels,
-            self.data_model.modules,
-        )
     
     def set_template(self,path):
         self.data_model.set_template(path)
@@ -38,25 +31,58 @@ class ReductusTemplateWidget:
             self.data_view.node_select.y = [self.data_model.nodes_y[self.selected_node]]
         
         self.data_view.show_module(
-            self.data_model.modules_meta[self.selected_node],
+            self.data_model.modules[self.selected_node],
             update_callback=self.update_template_config
         )
-    
+        
+    def update_node_status(self,*args):
+        colors = []
+        for i,module in enumerate(self.data_model.modules):
+            if (i in self.data_model.input_nodes) and (len(module['config']['filelist'])==0):
+                colors.append('red')
+            else:
+                colors.append('green')
+                
+        #update plot
+        with self.data_view.graph.batch_update():
+            self.data_view.node_status.marker.color = colors
+                
     def update_template_config(self,update):
         if update['type']!='change':
             return
-        print(update)
         
         value = update['new']
         param = update['owner'].description
         
-        self.data_model.modules_meta[self.selected_node]['config'][param] = value
+        self.data_model.modules[self.selected_node]['config'][param] = value
             
     def run(self):
+        self.data_view.init_nodes(
+            self.data_model.nodes_x,
+            self.data_model.nodes_y,
+            self.data_model.labels,
+            self.data_model.module_names,
+        )
+        self.data_view.init_edges(
+            self.data_model.edges_x,
+            self.data_model.edges_y,
+        )
+        self.data_view.init_node_select(
+            self.data_model.nodes_x[0],
+            self.data_model.nodes_y[0],
+        )
+        self.data_view.init_node_status(
+            self.data_model.nodes_x,
+            self.data_model.nodes_y,
+            self.data_model.input_nodes,
+        )
+        self.data_view.init_filelist()
+        self.data_view.build_graph()
+        
         widget = self.data_view.run()
         self.data_view.nodes.on_click(self.update_point)
         self.data_view.show_module(
-            self.data_model.modules_meta[self.selected_node],
+            self.data_model.modules[self.selected_node],
             update_callback=self.update_template_config
         )
         
@@ -69,68 +95,67 @@ class ReductusTemplateWidget_DataModel:
     def set_template(self,path):
         with open(path, 'rt') as f: 
             self.template = json.loads(f.read())
-        self.modules_meta = self.template['modules']
+        self.modules = self.template['modules']
         self.wires   = self.template['wires']
         
-        for m in self.modules_meta:
+        # flip the coordinate system to make visualization easier
+        for m in self.modules:
             m['y']*=-1
             
-        self.nodes_x = [m['x'] for m in self.modules_meta]
-        self.nodes_y = [m['y'] for m in self.modules_meta]
-        self.labels = [m['title'].split('.')[-1].replace(' ','\n') for m in self.modules_meta]
-        self.modules = [m['module'] for m in self.modules_meta]
-        
-        
+        self.nodes_x = [m['x'] for m in self.modules]
+        self.nodes_y = [m['y'] for m in self.modules]
+        self.labels = [m['title'].split('.')[-1].replace(' ','\n') for m in self.modules]
+        self.module_names = [m['module'] for m in self.modules]
         
         self.edges_x = []
         self.edges_y = []
+        self.input_nodes = np.ones(len(self.modules),dtype=bool)
         for w in self.wires:
             i0 = w['source'][0]
             i1 = w['target'][0]
-            x0 = self.modules_meta[i0]['x']
-            x1 = self.modules_meta[i1]['x']
-            y0 = self.modules_meta[i0]['y']
-            y1 = self.modules_meta[i1]['y']
+            x0 = self.modules[i0]['x']
+            x1 = self.modules[i1]['x']
+            y0 = self.modules[i0]['y']
+            y1 = self.modules[i1]['y']
             self.edges_x.extend([x0,x1,None])
             self.edges_y.extend([y0,y1,None])
+            
+            self.input_nodes[i1] = False 
+        
+        self.input_nodes = np.where(self.input_nodes)[0]
     
 class ReductusTemplateWidget_DataView:
     def __init__(self):
         self.widget = None
-    def init_graph(self,nodes_x,nodes_y,edges_x,edges_y,labels,modules):
-        modules_u = np.unique(modules)
-        cmap = matplotlib.cm.get_cmap('jet',modules_u.shape[0])
-        cmap = {m:matplotlib.colors.to_hex(cmap(i)) for i,m in enumerate(modules_u)}
-        colors = [cmap[m] for m in modules]
+        self.nodes = None
+        self.edges = None
+        self.node_select = None
+        self.node_status = None
         
-        nodes = go.Scatter(
-            x=nodes_x, y=nodes_y,
-            mode='markers',
-            hoverinfo='text',
-            marker=dict(
-                showscale=False,
-                size=20,
-                line_width=2,
-            )
+    def init_filelist(self):
+        
+        grid_options = {
+            'columnDefs':[{'field':'InputFile'}],
+            'enableSorting': False,
+            'enableFilter': False,
+            'enableColResize': False,
+            'rowSelection':'multiple',
+        }
+        self.filelist = ipyaggrid.Grid(
+            grid_data=[],
+            height=100,
+            grid_options=grid_options,
+            index=False,
+            quick_filter=False, 
+            export_mode='auto',
+            show_toggle_edit=False,
+            sync_grid=True,
+            sync_on_edit=True,
+            theme='ag-theme-balham', 
         )
-        nodes.text = labels
-        nodes.marker.color = colors
-        
-        node_select = go.Scatter(
-            x=[nodes_x[0]],y=[nodes_y[0]],
-            mode='markers',
-            hoverinfo='skip',
-            marker=dict(
-                showscale=False,
-                color='red',
-                size=35,
-                line_width=4,
-                symbol='circle-open',
-            ),
-            
-        )
-        
-        edges = go.Scatter(
+    
+    def init_edges(self,edges_x,edges_y):
+        self.edges = go.Scatter(
             x=edges_x, y=edges_y,
             mode='lines',
             line=dict(
@@ -142,10 +167,64 @@ class ReductusTemplateWidget_DataView:
             hoverinfo='none',
         )
         
-        xrange = [min(nodes_x)-40,max(nodes_x)+40]
-        yrange = [min(nodes_y)-30,max(nodes_y)+30]
+    def init_node_select(self,x,y):
+        self.node_select = go.Scatter(
+            x=[x],y=[y],
+            mode='markers',
+            hoverinfo='skip',
+            marker=dict(
+                showscale=False,
+                color='black',
+                size=30,
+                line_width=2,
+                symbol='circle-open',
+            ),
+            
+        )
+        
+    def init_node_status(self,nodes_x,nodes_y,input_nodes):
+        colors = ['red' if i in input_nodes else 'green' for i,_ in enumerate(nodes_x) ]
+        self.node_status = go.Scatter(
+            x=nodes_x,
+            y=nodes_y,
+            mode='markers',
+            hoverinfo='skip',
+            marker=dict(
+                showscale=False,
+                color=colors,
+                size=25,
+                line_width=4,
+                symbol='circle-open',
+            ),
+            
+        )
+        
+        
+    def init_nodes(self,nodes_x,nodes_y,labels,module_names):
+        module_names_u = np.unique(module_names)
+        cmap = matplotlib.cm.get_cmap('jet',module_names_u.shape[0])
+        cmap = {m:matplotlib.colors.to_hex(cmap(i)) for i,m in enumerate(module_names_u)}
+        colors = [cmap[m] for m in module_names]
+        
+        self.nodes = go.Scatter(
+            x=nodes_x, y=nodes_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=False,
+                size=20,
+                line_width=2,
+            )
+        )
+        self.nodes.text = labels
+        self.nodes.marker.color = colors
+        
+        
+    def build_graph(self):
+        xrange = [min(self.nodes.x)-50,max(self.nodes.x)+50]
+        yrange = [min(self.nodes.y)-30,max(self.nodes.y)+30]
         self.graph = go.FigureWidget(
-            data=[edges,nodes,node_select],
+            data=[self.edges,self.nodes,self.node_select,self.node_status],
             layout = dict(
                 width=500,
                 height=350,
@@ -156,9 +235,11 @@ class ReductusTemplateWidget_DataView:
                 plot_bgcolor='white',
             ),
         )
+        # need to reassign pointers because FigureWidget makes a copy...
+        self.edges = self.graph.data[0]
         self.nodes = self.graph.data[1]
         self.node_select = self.graph.data[2]
-        self.edges = self.graph.data[0]
+        self.node_status = self.graph.data[3]
         
         
     def show_module(self,module,update_callback=None):
@@ -191,9 +272,8 @@ class ReductusTemplateWidget_DataView:
         
         
         if 'filelist' in module['config']:
-            config_widgets.append(
-                ipywidgets.Textarea(description='filelist',value='')
-            )
+            self.filelist.update_grid_data([{'InputFile':v} for v in module['config']['filelist']])
+            config_widgets.append(self.filelist)
         self.config_vbox.children = config_widgets
                 
             
